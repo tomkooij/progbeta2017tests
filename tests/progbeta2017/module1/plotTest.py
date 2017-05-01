005382b7dfa2bb21528553b5e25355dec0e44603
jelleas <jelle.van.assema@gmail.com>
2017-05-01 15:25:57 +0200

    plotTest test niet meer op nulpunten

--- tests/progbeta2017/module1/plotTest.py
+++ tests/progbeta2017/module1/plotTest.py
@@ -6,9 +6,3 @@ import checkpy.assertlib as assertlib
 def showsGraph(test):
 	test.test = lambda : assertlib.fileContainsFunctionCalls(_fileName, "savefig")
 	test.description = lambda : "slaat een grafiek op"
-
-@t.test(1)
-def exactZeroPoint(test):
-	test.test = lambda : assertlib.numberOnLine(0.37, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.01)\
-				     and assertlib.numberOnLine(0.69, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.01)
-	test.description = lambda : "print het nulpunt (x = 0.37 en y = 0.69)"
