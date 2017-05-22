b0fd2e27d4c3f99bf2f94b74752d08e07f60c83a
Jelleas <jelle.van.assema@gmail.com>
2017-05-22 14:26:48 +0200

    appeltest

---
+++ tests/progbeta2017/module3/appelTest.py
@@ -0,0 +1,26 @@
+import checkpy.tests as t
+import checkpy.lib as lib
+import checkpy.assertlib as assertlib
+
+@t.test(0)
+def containsRequiredFunctionDefinitions(test):
+	test.test = lambda : assertlib.fileContainsFunctionDefinitions(_fileName, "appel")
+	test.description = lambda : "definieert de functie `appel()`"
+
+@t.passed(containsRequiredFunctionDefinitions)
+@t.test(1)
+def correctTime(test):
+	test.test = lambda : assertlib.numberOnLine(4.51, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.01)
+	test.description = lambda : "print het tijdstip waarop de appel de grond raakt"
+
+@t.passed(containsRequiredFunctionDefinitions)
+@t.test(2)
+def correctSpeed(test):
+	test.test = lambda : assertlib.numberOnLine(159.5, lib.getLine(lib.outputOf(_fileName), 1), deviation = 0.1)
+	test.description = lambda : "print de snelheid waarmee de appel de grond raakt"
+
+@t.passed(containsRequiredFunctionDefinitions)
+@t.test(3)
+def correctSecondsToHit100(test):
+	test.test = lambda : assertlib.numberOnLine(2.83, lib.getLine(lib.outputOf(_fileName), 2), deviation = 0.01)
+	test.description = lambda : "print het tijdstip waarop een snelheid van 100km/u wordt bereikt"
