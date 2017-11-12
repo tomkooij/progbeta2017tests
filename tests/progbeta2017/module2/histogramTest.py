fd3959c01d10549a962a8a0536eade116855637d
jelleas <jelle.van.assema@gmail.com>
2017-11-12 21:18:39 +0100

    remove debug print

--- tests/progbeta2017/module2/histogramTest.py
+++ tests/progbeta2017/module2/histogramTest.py
@@ -18,7 +18,6 @@ def after():
 
 @t.test(0)
 def hasSomRandomGetallen(test):
-	print _fileName
 	test.test = lambda : assertlib.fileContainsFunctionDefinitions(_fileName, "SomRandomGetallen")
 	test.description = lambda : "definieert de functie SomRandomGetallen"
 
@@ -26,7 +25,6 @@ def hasSomRandomGetallen(test):
 @t.passed(hasSomRandomGetallen)
 @t.test(10)
 def correctBelow40(test):
-
 	tsts = ['40', 'veertig', 'forty']
 	test.test = lambda : assertlib.numberOnLine(6, lib.getLine(lib.outputOf(_fileName), 0), deviation = 5) and sum([assertlib.contains(lib.outputOf(_fileName), tst) for tst in tsts])
 	test.description = lambda : "print hoe vaak de som minder dan 40 is"
