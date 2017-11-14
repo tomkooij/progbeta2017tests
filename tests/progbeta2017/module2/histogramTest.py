627aad2f9454a8ceace41d211073419f94dcab39
Vera Schild <veraschild@gmail.com>
2017-11-14 12:31:01 +0100

    Change in deviation and output of histogramTest.py

--- tests/progbeta2017/module2/histogramTest.py
+++ tests/progbeta2017/module2/histogramTest.py
@@ -18,7 +18,6 @@ def after():
 
 @t.test(0)
 def hasSomRandomGetallen(test):
-	print _fileName
 	test.test = lambda : assertlib.fileContainsFunctionDefinitions(_fileName, "SomRandomGetallen")
 	test.description = lambda : "definieert de functie SomRandomGetallen"
 
@@ -26,17 +25,16 @@ def hasSomRandomGetallen(test):
 @t.passed(hasSomRandomGetallen)
 @t.test(10)
 def correctBelow40(test):
-
 	tsts = ['40', 'veertig', 'forty']
-	test.test = lambda : assertlib.numberOnLine(6, lib.getLine(lib.outputOf(_fileName), 0), deviation = 5) and sum([assertlib.contains(lib.outputOf(_fileName), tst) for tst in tsts])
-	test.description = lambda : "print hoe vaak de som minder dan 40 is"
+	test.test = lambda : (assertlib.numberOnLine(5, lib.getLine(lib.outputOf(_fileName), 0), deviation = 5) or assertlib.numberOnLine(0.05, lib.getLine(lib.outputOf(_fileName), 0), deviation = 0.05)) and sum([assertlib.contains(lib.outputOf(_fileName), tst) for tst in tsts])
+	test.description = lambda : "print op de eerste regel hoe vaak de som minder dan 40 is"
 
 @t.passed(hasSomRandomGetallen)
 @t.test(20)
 def correctAbove60(test):
 	tsts = ['60', 'zestig', 'sixty']
-	test.test = lambda : assertlib.numberOnLine(6, lib.getLine(lib.outputOf(_fileName), 1), deviation = 5) and sum([assertlib.contains(lib.outputOf(_fileName), tst) for tst in tsts])
-	test.description = lambda : "print hoe vaak de som meer dan 60 is"
+	test.test = lambda : (assertlib.numberOnLine(5, lib.getLine(lib.outputOf(_fileName), 1), deviation = 5) or assertlib.numberOnLine(0.05, lib.getLine(lib.outputOf(_fileName), 1), deviation = 0.05))and sum([assertlib.contains(lib.outputOf(_fileName), tst) for tst in tsts])
+	test.description = lambda : "print op de tweede regel hoe vaak de som meer dan 60 is"
 
 
 
