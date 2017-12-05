9f672d015dda1d2132c035aa55f68304d22fa16c
Vera Schild <veraschild@gmail.com>
2017-12-05 20:50:19 +0100

    Timeout verhoogd en output hoeft niet meer op een bepaalde regel

--- tests/progbeta2017/module3/monopoly_realistischTest.py
+++ tests/progbeta2017/module3/monopoly_realistischTest.py
@@ -16,44 +16,59 @@ def after():
 @t.test(0)
 def hassimuleer_groot_aantal_potjes_Monopoly(test):
 	def try_run():
-		try:	
-			testInput = lib.getFunction("simuleer_groot_aantal_potjes_Monopoly", _fileName)(1000000, 1000000)
-			return True
-		except:
-			return False
+		if assertlib.fileContainsFunctionDefinitions(_fileName, "simuleer_groot_aantal_potjes_Monopoly"):
+			try:	
+				testInput = lib.getFunction("simuleer_groot_aantal_potjes_Monopoly", _fileName)(1000000, 1000000)
+				return True
+			except:
+				return False
+		return False
 
-	test.test = lambda : (assertlib.fileContainsFunctionDefinitions(_fileName, "simuleer_groot_aantal_potjes_Monopoly") and try_run())
-
-	
+	test.test = try_run
 	test.fail = lambda info : "zorg dat de functie twee argumenten heeft, startgeld voor speler 1 en startgeld voor speler 2"
-	test.description = lambda : "definieert de functie simuleer_potjeAndsimuleer_groot_aanal_potjes_Monopoly met twee argumenten"
-	test.timeout = lambda : 60
+	test.description = lambda : "definieert de functie simuleer_potje en simuleer_groot_aanal_potjes_Monopoly met twee argumenten"
+	test.timeout = lambda : 90
 
 
 @t.passed(hassimuleer_groot_aantal_potjes_Monopoly)
 @t.test(10)
 def correctAverageDiv(test):
+	def testMethod():
+		outcome = lib.getFunction("simuleer_groot_aantal_potjes_Monopoly", _fileName)(1500, 1500)
+		if assertlib.sameType(outcome, None):
+			info = "Zorg er voor dat de functie simuleer_groot_aantal_potjes_Monopoly het verschil in het bezit van straten returnt en alleen deze waarde returnt"
+		elif assertlib.between(outcome, 0, 99999999):
+			info = "Als speler 1 meer straten heeft dan speler 2 is het verschil negatief"
+		else:
+			info = "Het verschil is niet erg groot, gemiddeld zelfs minder dan 1 straat"
+		return assertlib.between(outcome, -.45, -.15), info
 
-	if assertlib.sameType(lib.getFunction("simuleer_groot_aantal_potjes_Monopoly", _fileName)(1500, 1500), None):
-		test.fail = lambda info : "Zorg er voor dat de functie simuleer_groot_aantal_potjes_Monopoly het verschil in het bezit van straten returnt en alleen deze waarde returnt"
-	elif assertlib.between(lib.getFunction("simuleer_groot_aantal_potjes_Monopoly", _fileName)(1500, 1500), 0, 99999999):
-		test.fail = lambda info : "Als speler 1 meer straten heeft dan speler 2 is het verschil negatief"
-	else:
-		test.fail = lambda info : "Het verschil is niet erg groot, gemiddeld zelfs minder dan 1 straat"
-
-	
-	test.test = lambda : assertlib.between(lib.getFunction("simuleer_groot_aantal_potjes_Monopoly", _fileName)(1500, 1500), -.45, -.15)
+	test.test = testMethod
 	test.description = lambda : "Monopoly met twee spelers geeft de het correcte gemiddelde verschil in gekochten straten"
-	test.timeout = lambda : 60
+	test.timeout = lambda : 90
 
 
 
 @t.passed(correctAverageDiv)
 @t.test(20)
 def correctAverageDiv(test):
-	test.test = lambda : assertlib.numberOnLine(125, lib.getLine(lib.outputOf(_fileName), 0))
+
+	def findline(outputOf):
+		tsts = ['startgeld', 'evenveel', 'straten']
+		for line in outputOf.split("\n"):
+			if all([assertlib.contains(line, tst) for tst in tsts]):
+				return line
+		return ""
+
+	line = findline(lib.outputOf(_fileName))
+	print line
+
+	if assertlib.numberOnLine(75, line):
+		test.fail = lambda info : "De gevonden waarde is 75 euro. Checkpy het programma nog een keer."
+
+	test.test = lambda : assertlib.numberOnLine(125, line)
 	test.description = lambda : "Monopoly met twee spelers vindt het correcte extra startgeld voor speler 2"
-	test.timeout = lambda : 60
+	test.timeout = lambda : 90
 
 
 
